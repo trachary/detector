2b553b7bea1b556423680eae638214710d75b21b
Zach Liou <zach@trackrevenue.com>
2016-06-27 16:53:05 -0700

    Add orientation detection

--- lab.js
+++ lab.js
@@ -3,27 +3,44 @@ console.log("Hello world!");
 console.log("Screen data:");
 console.log("width: " + screen.width);
 console.log("height: " + screen.height);
-alert(screen.width + " x " + screen.height);
+//alert(screen.width + " x " + screen.height);
 
-navigator.getBattery().then(function(battery) {
-  console.log(battery.level);
-  document.write(battery.level);
-  document.write("<br/>");
+
+window.addEventListener("deviceorientation", handleOrientation, true);
+
+function handleOrientation(event) {
+  var absolute = event.absolute;
+  var alpha    = event.alpha;
+  var beta     = event.beta;
+  var gamma    = event.gamma;
+
+  // Do stuff with the new orientation data
+  console.log("alpha: " + alpha
+  	+ "\nbeta: " + beta
+  	+ "\ngamma: " + gamma
+  );
+  document.write("abg:" + alpha + " " + beta + " " + gamma );
+}
+
+// navigator.getBattery().then(function(battery) {
+//   console.log(battery.level);
+//   document.write(battery.level);
+//   document.write("<br/>");
   
-  // ... and any subsequent updates.
-  battery.onlevelchange = function() {
-    console.log(this.level);
-    document.write(this.level);
-  };
+//   // ... and any subsequent updates.
+//   battery.onlevelchange = function() {
+//     console.log(this.level);
+//     document.write(this.level);
+//   };
 
-  battery.onchargingchange = function() {
-  	if (this.charging) {
-  		console.log("plugged in");
-  		document.write("plugged in<br/>");
-  	} else  {
-  		console.log("unplugged");
-  		document.write("unplugged<br/>");
-  	}
-  }
+//   battery.onchargingchange = function() {
+//   	if (this.charging) {
+//   		console.log("plugged in");
+//   		document.write("plugged in<br/>");
+//   	} else  {
+//   		console.log("unplugged");
+//   		document.write("unplugged<br/>");
+//   	}
+//   }
 
-});
+// });
