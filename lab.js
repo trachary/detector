6340db3d93dbc844455934c4df90e7cff75175f5
Zach Liou <zach@trackrevenue.com>
2016-06-27 16:27:41 -0700

    Add charge state change detection

--- lab.js
+++ lab.js
@@ -2,10 +2,17 @@ console.log("Hello world!");
 
 navigator.getBattery().then(function(battery) {
   console.log(battery.level);
-  document.write(battery.level);
+  document.write(battery.level + "<br/>");
+  
   // ... and any subsequent updates.
   battery.onlevelchange = function() {
     console.log(this.level);
     document.write(this.level);
   };
+
+  battery.onchargingchange = function() {
+  	if (this.charging) console.log("plugged in");
+  	else console.log("unplugged");
+  }
+
 });
