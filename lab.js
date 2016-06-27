0fa96b1f03ded1e30fd65230e623243eeea3cbe3
Zach Liou <zach@trackrevenue.com>
2016-06-27 16:37:58 -0700

    Update document writes

--- lab.js
+++ lab.js
@@ -2,7 +2,8 @@ console.log("Hello world!");
 
 navigator.getBattery().then(function(battery) {
   console.log(battery.level);
-  document.write(battery.level + "<br/>");
+  document.write(battery.level);
+  document.write("<br/>");
   
   // ... and any subsequent updates.
   battery.onlevelchange = function() {
@@ -11,8 +12,13 @@ navigator.getBattery().then(function(battery) {
   };
 
   battery.onchargingchange = function() {
-  	if (this.charging) console.log("plugged in");
-  	else console.log("unplugged");
+  	if (this.charging) {
+  		console.log("plugged in");
+  		document.write("plugged in<br/>");
+  	} else  {
+  		console.log("unplugged");
+  		document.write("unplugged<br/>");
+  	}
   }
 
 });
