1a479941a0278923ceff6134dd4bf81703d4b3d6
Zach Liou <zach@trackrevenue.com>
2016-06-27 17:25:11 -0700

    Add window size detection

--- lab.js
+++ lab.js
@@ -1,24 +1,33 @@
 console.log("Hello world!");
 
-console.log("Screen data:");
-console.log("width: " + screen.width);
-console.log("height: " + screen.height);
+console.log("Screen data:"
+	+ "\nscreen width: " + screen.width
+	+ "\nscreen height: " + screen.height
+	+ "\nwindow width: " + window.innerWidth
+	+ "\nwindow height: " + window.innerHeight);
+
+document.write("screen width: " + screen.width
+	+ "<br/>screen height: " + screen.height
+	+ "<br/>window width: " + window.innerWidth
+	+ "<br/>window height: " + window.innerHeight);
+
 //alert(screen.width + " x " + screen.height);
 
 
 window.addEventListener("deviceorientation", handleOrientation, true);
 
 function handleOrientation(event) {
-  var absolute = event.absolute;
-  var alpha    = event.alpha;
-  var beta     = event.beta;
-  var gamma    = event.gamma;
+	var absolute = event.absolute;
+	var alpha    = event.alpha;
+	var beta     = event.beta;
+	var gamma    = event.gamma;
 
   // Do stuff with the new orientation data
-  console.log("alpha: " + alpha
-  	+ "\nbeta: " + beta
-  	+ "\ngamma: " + gamma
-  );
+	console.log("Orientation data:"
+		+ "\nalpha: " + alpha
+		+ "\nbeta: " + beta
+		+ "\ngamma: " + gamma
+	);
   //document.write("abg: " + alpha + " " + beta + " " + gamma );
 
   // if (alpha == null && beta == null && gamma == null) {
@@ -28,14 +37,14 @@ function handleOrientation(event) {
   // }
 }
 
-  document.addEventListener("click", function( event ) {
+document.addEventListener("click", function( event ) {
     // display the current click count inside the clicked div
-    document.write("click:"
+    console.log("click:"
     	+ "\nx: " + event.screenX 
     	+ "\ny: " + event.screenY
-    	+ "\npressure: " + event.mozPressure
     );
-  }, false);
+}, false);
+
 
 
 // navigator.getBattery().then(function(battery) {
