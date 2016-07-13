6bc98992bd557b5f63baccdd5237ed0480e56132
Zach Liou <zach@trackrevenue.com>
2016-07-13 12:05:12 -0700

    Add screen ratio calculation

--- screenLab.js
+++ screenLab.js
@@ -2,12 +2,44 @@
 
     "use strict";
 
+    // Get HTML elements
     var sizes = document.querySelector('.sizes');
 
-    // Get screen data
-    sizes.innerHTML = "screen width: " + screen.width
-        + "\nscreen height: " + screen.height
-        + "\nwindow width: " + window.innerWidth
-        + "\nwindow height: " + window.innerHeight;
+    // Init constants
+    var RATIO_16_BY_9 = (16.0 / 9.0);
+    var RATIO_3_BY_2 = (3.0 / 2.0);
+
+    // Init vars
+    var ratio = screen.width / screen.height;
+
+    /*
+     * Get data
+     */
+    getScreenData();
+    getRatio();
+
+
+    /*
+     * Functions below
+     */
+
+    function getScreenData() {
+        sizes.innerHTML = "screen width: " + screen.width
+            + "\nscreen height: " + screen.height
+            + "\n"
+            + "\nwindow width: " + window.innerWidth
+            + "\nwindow height: " + window.innerHeight;
+    }
+
+    function getRatio() {
+        sizes.innerHTML += "\n\n"
+            + "ratio: " + ratio;
+
+        if (ratio == RATIO_16_BY_9) {
+            sizes.innerHTML += " (16:9)";
+        } else if (ratio == RATIO_3_BY_2) {
+            size.innerHTML += " (3:2)";
+        }
+    }
 
 })();
