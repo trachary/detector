5646205a00f2e2a81c0917640329f49be8b10c20
Zach Liou <zach@trackrevenue.com>
2016-08-15 18:21:40 -0700

    Change rules for iOS

--- lenient.js
+++ lenient.js
@@ -191,6 +191,11 @@
     		return;
     	}
 
+    	if (IS_IOS) {
+    		IS_MOBILE = (!HAS_BATTERY || HAS_FULL_BATTERY || IS_CHARGING);
+    		return;
+    	}
+
     	if (!HAS_VALID_LANG || !HAS_BATTERY || HAS_FULL_BATTERY || IS_CHARGING) {
     		IS_MOBILE = false;
     		return;
@@ -201,10 +206,10 @@
     		return;
     	}
 
-    	if (IS_IOS) {
-    		IS_MOBILE = !HAS_COMMON_ASPECT_RATIO;
-    		return;
-    	}
+    	// if (IS_IOS) {
+    	// 	IS_MOBILE = !HAS_COMMON_ASPECT_RATIO;
+    	// 	return;
+    	// }
 
     	IS_MOBILE = true;
     }
