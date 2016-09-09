b250eb24240272e736fab6fec3bf3bb723221b8e
Zach Liou <zach@trackrevenue.com>
2016-09-09 12:55:34 -0700

    Refine

--- refine_no_debug.js
+++ refine_no_debug.js
@@ -1,41 +1,54 @@
-/* File: refine.js
+/* 
+ * File: refine.js
+ * Author: Zachary Liou
  * Date created: 2016 August 24
- * Description: lenient/simple version of detect.js
+ * Description: refined/simplified version of lenient.js
  */
 
 
 
-var start = Date.now();
+/*
+ * Init vars
+ */
 
-
+// General detection vars
 var IS_MOBILE       = false;
+var DECIDED         = false;
 
+// Timing vars
 var TIME_LIMIT 		= 10;
 var TIME_BATTERY    = 75;
 var TIME_GYRO_AND   = 300;
 var TIME_GYRO_IOS   = 500;
-var DECIDED 		= false;
 
-var HAS_BATTERY 	= false;
-var HAS_FULL_BATTERY = false;
-var IS_CHARGING     = false;
-
-var HAS_GYRO 		= false;
-
-var UA_MOB          = false;
-var UA_AND          = false;
-var UA_IOS 			= false;
-
+// Detection mode flags
 var MODE_BATTERY    = false;
 var MODE_GYRO       = false;
 
+// UA String flags
+var UA_MOB          = false;
+var UA_AND          = false;
+var UA_IOS          = false;
+
+// Battery flags
+var BATT_ACCESS 	= false;
+var BATT_FULL       = false;
+var BATT_CHARGING   = false;
+
+// Gyro flags
+var GYRO_EXISTS 	= false;
+
+
+
+/*
+ * Begin detection procedure
+ */
 
 
 checkUAString();
 setModes(CONFIG);       // CONFIG should be set by dz.php
+setTimeLimit();       
 
-setTimeLimit();
-var timeoutID = window.setTimeout(detectDevice, TIME_LIMIT);
 
 if (MODE_GYRO) {
     window.addEventListener("deviceorientation", handleOrientation, true);
@@ -46,12 +59,38 @@ if (MODE_BATTERY) {
 }
 
 
+/*
+ * Functions below
+ */
+
+/*
+ * 
+ */
+function setModes(config) {
+
+    // Simple profile; also used for invalid or missing config param
+    if (config == null || config <= 1) {
+        MODE_BATTERY = true;
+        MODE_GYRO = UA_AND;  // Use gyro only for Android
+
+    // Advanced profile
+    } else {
+        var MASK_BATTERY = 2;
+        var MASK_GYRO = 1;
+
+        MODE_BATTERY = config & MASK_BATTERY;
+        MODE_GYRO = config & MASK_GYRO;
+    }
+
+}
+
+
+
 
 function checkBattery(battery) {
-    var end = Date.now();
-    HAS_BATTERY = true;
-    IS_CHARGING = battery.charging;
-    HAS_FULL_BATTERY = battery.level == 1;
+    BATT_ACCESS = true;
+    BATT_CHARGING = battery.charging;
+    BATT_FULL = battery.level == 1;
 }
 
 
@@ -67,18 +106,11 @@ function checkUAString() {
 
 
 function handleOrientation(event) {
-	HAS_GYRO = (event.alpha != null || event.beta != null || event.gamma != null);
+	GYRO_EXISTS = (event.alpha != null || event.beta != null || event.gamma != null);
 }
 
 
 
-function setModes(config) {
-    var MASK_BATTERY = 2;
-    var MASK_GYRO = 1;
-
-    MODE_BATTERY = config & MASK_BATTERY;
-    MODE_GYRO = config & MASK_GYRO;
-}
 
 
 
@@ -94,6 +126,9 @@ function setTimeLimit() {
             TIME_LIMIT += TIME_BATTERY;
         }
     }
+
+    // Detect device when timer limit is reached
+    var timeoutID = window.setTimeout(detectDevice, TIME_LIMIT);
 }
 
 
@@ -104,12 +139,16 @@ function decide() {
 
 	if (!UA_MOB) {
 		IS_MOBILE = false;
+
 	} else if (UA_IOS) {
-		IS_MOBILE = !HAS_BATTERY;
-	} else if (!HAS_BATTERY || HAS_FULL_BATTERY || IS_CHARGING) {
+		IS_MOBILE = !BATT_ACCESS;
+
+	} else if (!BATT_ACCESS || BATT_FULL || BATT_CHARGING) {
 		IS_MOBILE = false;
+
 	} else if (UA_AND) {
-		IS_MOBILE = HAS_GYRO;
+		IS_MOBILE = GYRO_EXISTS;
+
 	} else {
         IS_MOBILE = false;
     }
@@ -122,7 +161,7 @@ function detectDevice() {
     if (!DECIDED) {
         DECIDED = true;
         decide();
-        result();
+        //result();
     }
 }
 
